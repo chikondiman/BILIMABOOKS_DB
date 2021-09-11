d747789d58e2ab11869ca17d06cf2ebe66662f92
Chikondi Kamvazaana <61444123+chikondiman@users.noreply.github.com>
2021-09-11 16:38:43 -0400

    update

---
+++ DBhouse/Customer.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Customer]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [first_name] NCHAR(10) NULL, 
+    [last_name] NCHAR(10) NULL, 
+    [book] NCHAR(10) NULL, 
+    [total] NCHAR(10) NULL
+)
---
+++ DBhouse/books.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Table1]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Book] TEXT NULL, 
+    [Title] TEXT NULL, 
+    [Price] INT NULL
+)
